fc39dfed71a99c683de827a909003b8698452851
Jordan Shehane <jordanshehane@gmail.com>
2021-02-09 23:55:21 -0500

    added searchbar to map, added state to map

--- src/components/common/Searchbar/SearchBar.js
+++ src/components/common/Searchbar/SearchBar.js
@@ -1,7 +1,10 @@
 import React, { useState, useContext } from 'react';
 import { Input } from 'antd';
 import locationOnSubmit from '../../../utils/locationSubmit';
+import setMapObject from '../../../utils/setMapObject';
 import { LocationContext } from '../../../state/contexts';
+import { useHistory } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const SearchBar = () => {
   const [searchVal, setSearchVal] = useState('');
@@ -12,14 +15,24 @@ const SearchBar = () => {
   };
 
   const { Search } = Input;
+  let l = useLocation();
+  let history = useHistory();
 
   return (
     <Search
+      className="SearchBar"
       placeholder="Search for a location"
       value={searchVal}
       onChange={handleChanges}
       enterButton
-      onSearch={() => locationOnSubmit(setLocation, searchVal)}
+      onSearch={() => {
+        if (l.pathname !== '/map') {
+          locationOnSubmit(setMapObject, setLocation, searchVal);
+          history.push('/map');
+        } else {
+          locationOnSubmit(setMapObject, setLocation, searchVal);
+        }
+      }}
     />
   );
 };
--- src/utils/locationSubmit.js
+++ src/utils/locationSubmit.js
@@ -1,19 +1,27 @@
 import axios from 'axios';
-
-const locationOnSubmit = (setter, value) => {
+// added a formater peram, that takes the function that formats the data for the component we are passing it to
+const locationOnSubmit = (formatter, state_setter, value) => {
   const reqArr = value.split(', ');
   const reqBody = {
     city_name: reqArr[0],
     state_name: reqArr[1],
   };
+  let info = null;
   axios
     .post('https://h-ds2.cityspire.dev/location', reqBody)
     .then(res => {
       console.log(res.data);
-      setter(JSON.parse(res.data)[0]);
+      //first we format the data
+      const new_value = formatter(JSON.parse(res.data)[0]);
+      //then pass it to state.
+      console.log(new_value);
+      info = new_value;
     })
     .catch(err => {
       console.error(err);
+    })
+    .finally(() => {
+      state_setter(info);
     });
 
   console.log('submitted');
